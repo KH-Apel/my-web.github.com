1f3a7e757a83f19e40624ce7841ae5c92615fca2
Md. Khaled Hasan Apel <apelhasan@gmail.com>
2017-12-05 01:58:18 +0600

    class-9

---
+++ class-9/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+    	
+      	$(".client-testimonial-carousel").owlCarousel({
+      		item: 3,
+      		margin: 30,
+      		nav: false,
+      		dots: true,
+      		loop: true
+      });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
